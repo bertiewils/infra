05fb4b19a027e4ed3ce60b89a7eeb5ab68708f1b
Bertie Wilson <19255166+bertiewils@users.noreply.github.com>
2021-06-22 05:37:02 +0100

    Move to hetzner

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -18,3 +18,23 @@ provider "registry.terraform.io/hashicorp/aws" {
     "zh:ebf9c5ef3eceebc1c21bcd31e535e5c323c3bf6ca5918959e297e9a6617d8094",
   ]
 }
+
+provider "registry.terraform.io/hetznercloud/hcloud" {
+  version = "1.27.0"
+  hashes = [
+    "h1:wzibawtXS0AOUCzEx778eE875kLOT0Z0XOmjDP7Ur5c=",
+    "zh:05c94762bc6006d3aed181d4192b95d17f803c56566731665aa18ecfd72ff6fa",
+    "zh:1b7846fd21b5f1c0230eaac54692729e03a1adda90e2f64bec8cba81dc523555",
+    "zh:46f91661b0f976f6280213f7f8af7125a7df1c90977ab87c628a384302cc89da",
+    "zh:58bd83dd11f1e5fff80d6ba6050172ebaa56977b6983abcdd1ea4a8e3746546e",
+    "zh:843b371274cbeda474c61b8d70291c6981ef26592ad577e6b8cc9cde6a081031",
+    "zh:88ca8df821f0fb99b35c1b84bcbf2b5625b75b2ab86a5d4aa4baed1199fa2a6a",
+    "zh:93bfc233acd33cf62ae70cb914f4c273ed024e25271261b7803f40b30197fb53",
+    "zh:aa6fe985368f53fc9a0f8256da928f22da522a4efb5ee564ebc42e2613ed2100",
+    "zh:af6a86598256acf81b91ffff7eafd26a9edc37b42e1ddbabc28b8a27339265a1",
+    "zh:c99c4aa1abb2e200cb5d8cd3612afacf1211cb5782c388d772165cfb02d318ce",
+    "zh:cc0990558867ab49715638f12bb9c8c4a3cd9ac63f45bff73177b36dd9467561",
+    "zh:e5a1fdde5f9516ba9b52dc31f4f25f82e5fe748bc400464375c259737a12a2da",
+    "zh:f965f1f8d797d100aa9dec633f94455fc86c6a76dad44c6969d11c6006f80143",
+  ]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
+    hcloud = {
+      source = "hetznercloud/hcloud"
       # version = "~> 3.46.0"
     }
   }
@@ -9,17 +9,67 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "eu-west-2"
+variable "hcloud_token" {}
+
+provider "hcloud" {
+  token = var.hcloud_token
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0194c3e07668a7e36"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
+resource "hcloud_firewall" "general" {
+  name = "General"
+  rule {
+    direction = "in"
+    protocol  = "icmp"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
   }
+}
+
+resource "hcloud_firewall" "ssh-home" {
+  name = "SSH Home"
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "22"
+    source_ips = [
+      "217.155.26.60/32"
+    ]
+  }
+}
+
+resource "hcloud_firewall" "webapp-public" {
+  name = "WebApp Public"
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "80"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_server" "docker-htz" {
+  name        = "docker-htz"
+  image       = "debian-10"
+  server_type = "cx11"
+  location    = "nbg1"
+  backups = false
+  ssh_keys = [
+    "bert@Berties-MBP.home",
+    "bert@shell"
+  ]
 }
 
