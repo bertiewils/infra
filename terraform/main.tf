2fb88b58edd60fc879ab5ab16f5e65cd09d522d0
Bertie Wilson <19255166+bertiewils@users.noreply.github.com>
2021-06-22 01:02:32 +0100

    Initial main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # version = "~> 3.46.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0194c3e07668a7e36"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
