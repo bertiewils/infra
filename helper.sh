612f139975c33d0c3927bcf91d552516a3103d50
Bertie Wilson <19255166+bertiewils@users.noreply.github.com>
2022-02-02 18:18:03 +0000

    Switch from Makefile to script

--- helper.sh
+++ helper.sh
@@ -4,38 +4,34 @@
 # Usage: ./helper.sh <command>
 #
 
-function help () # Show this help
-{
+function help () { # Show this help
 	echo "Options:"
   grep "^function" "$0" \
-  | awk '{out=$2; for(i=4;i<=NF;i++){out=out" "$i}; print out}' \
+  | awk '{out=$2; for(i=5;i<=NF;i++){out=out" "$i}; print out}' \
   | sort \
   | awk 'BEGIN {FS = " # "}; {printf "\033[36m%-20s\033[0m %s\n", $1, $2}'
 }
 
-function creds () # Export creds as env vars
-{
+function creds () { # Ready the credentials vault
   local CREDS_FILE=credentials.vault
 	grep -q "ANSIBLE_VAULT" ${CREDS_FILE} && ansible-vault decrypt ${CREDS_FILE}
   echo "Run: source ${CREDS_FILE}"
 }
 
-function decrypt () # Decrypt all vaults
-{
+function decrypt () { # Decrypt all vaults
   ansible-vault decrypt \
     group_vars/all/vault.yml \
     credentials.vault
 }
 
-function encrypt () # Encrypt all vaults
-{
+function encrypt () { # Encrypt all vaults
   ansible-vault encrypt \
     group_vars/all/vault.yml \
     credentials.vault
 }
 
-function reqs () # Install requirements
-{
+function reqs () { # Install requirements
+  # add checks for terraform and ansible
   pip install passlib  # for password_hash
 	ansible-galaxy install -r requirements.yml
 }
